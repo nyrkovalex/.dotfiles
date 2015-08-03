b564e9df4c593e32a0f22abd508371e93933c4fc
Alexander Nyrkov <nyrkovalex@gmail.com>
2015-08-04 00:08:35 +0300

    fixed mkdir -p, i3 paths

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 # X stuff
 ln -sf ~/.dotfiles/Xmodmap ~/.Xmodmap
 ln -sf ~/.dotfiles/Xresources ~/.Xresources
+ln -sf ~/.dotfiles/Xresources ~/.Xdefaults
 
 # i3
-mkdirp ~/.i3
+mkdir -p ~/.i3
 ln -sf ~/.dotfiles/i3status.conf ~/.i3status.conf
-ln -sf ~/.dotfiles/i3/config ~/.i3-config
+ln -sf ~/.dotfiles/i3-config ~/.i3/config
 
 # wmii
-mkdirp ~/.wmii
+mkdir -p ~/.wmii
 ln -sf ~/.dotfiles/wmiirc ~/.wmii/wmiirc
 
 # VIM
