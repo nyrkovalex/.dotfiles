a6f144dc798c686041c6d56a23f4448c01e2e299
Alexander Nyrkov <nyrkovalex@gmail.com>
2017-06-05 02:49:28 +0300

    i3 config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# VIM
-# sh ~/.dotfiles/vimify.sh
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/Xresources ~/.Xresources
+ln -s ~/.dotfiles/i3 ~/.config/
+ln -s ~/.dotfiles/i3status ~/.config/
