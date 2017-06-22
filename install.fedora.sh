90621c4d137c30d44c9d7818cbb5fd9e977dbdef
Alexander Nyrkov <nyrkovalex@gmail.com>
2017-06-22 19:29:26 +0300

    Postgres @ install script

--- install.fedora.sh
+++ install.fedora.sh
@@ -1,4 +1,20 @@
 dconf write /org/gnome/desktop/input-sources/xkb-options "['compose:ralt']" \
   && sudo dnf group install "C Development Tools and Libraries" \
-  && sudo dnf install cmake python-devel tmux xclip gimp inkscape nodejs java-1.8.0-openjdk java-1.8.0-openjdk-src gnome-terminal-nautilus liberation-fonts the_silver_searcher ranger
+  && sudo dnf install \
+    cmake \
+    python-devel \
+    tmux \
+    xclip \
+    gimp \
+    inkscape \
+    nodejs \
+    java-1.8.0-openjdk \
+    java-1.8.0-openjdk-src \
+    gnome-terminal-nautilus \
+    liberation-fonts \
+    the_silver_searcher \
+    ranger \
+    postgresql-server \
+    postgresql-devel \
+    redhat-rpm-config
 
