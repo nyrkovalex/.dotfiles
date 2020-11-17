940c2e0ec839c6a26606c01474e00c29dcf04764
Alexander Nyrkov <nyrkovalex@gmail.com>
2020-11-17 15:54:03 +0300

    update vimify script

--- vimify.sh
+++ vimify.sh
@@ -2,28 +2,9 @@
 
 VIM_HOME=~/.config/nvim
 
-install_pathogen () {
-  mkdir -p $VIM_HOME/autoload ~/.dotfiles/bundle
-  curl -LSso $VIM_HOME/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-}
-
-install_bundles () {
-  cd ~/.dotfiles
-  git submodule init
-  git submodule update --init --recursive
-}
-
-build_ycm () {
-  cd ~/.dotfiles/bundle/YouCompleteMe
-  ./install.py
-}
-
-mkdir -p $VIM_HOME
-install_pathogen
-install_bundles
-build_ycm
-
 cd $VIM_HOME
-ln -sf ~/.dotfiles/bundle
 ln -sf ~/.dotfiles/vimrc ./init.vim
 touch ~/.dotfiles/local.vim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm installer.sh
